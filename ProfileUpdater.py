ca1c78a7b7ad8719b260226ea80a911b94baf966
OQN-LL <luta00000@gmail.com>
2019-08-02 22:55:44 +0900

    更新

--- ProfileUpdater.py
+++ ProfileUpdater.py
@@ -6,5 +6,5 @@ import re
 if __name__ == "__main__":
     with open("profile_list.txt", "r") as f:
         names = re.sub(r"\n$", "", f.read()).split("\n")
-    print(choice(names))
-    # update_name("test")
+    #print(choice(names))
+    update_name("test")
